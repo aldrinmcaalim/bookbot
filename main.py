a04d0c3c4480a7b4c17d96572d061c013175ecd2
aldrinmcaalim <aldrinmcaalim@gmail.com>
2022-10-25 02:46:43 -0400

    made function that gets total word count

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ book_path = "books/frankenstein.txt"
 def main():
     text = get_book_text(book_path)
     print(text)
+    print(f"There are a total of {get_word_count(text)} words in the document!")
 
 def get_book_text(book_path):
     with open(book_path) as bp:
         book_contents = bp.read()
     return book_contents
+
+def get_word_count(text):
+    total_word_count = len(text.split())
+    return total_word_count
+
 
 main()
 
