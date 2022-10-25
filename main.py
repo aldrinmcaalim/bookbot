0ff5ccad1a483887a324a6bab657076b7d5c8d7d
aldrinmcaalim <aldrinmcaalim@gmail.com>
2022-10-25 03:35:45 -0400

    added sort function

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ book_path = "books/frankenstein.txt"
 def main():
     text = get_book_text(book_path)
     print(text)
-    #print(count_letters(text))
+    print(count_letters(text))
     print(f"----- Begin Report of {book_path} -----")
     print(f"There are a total of {get_word_count(text)} words in the document!")
     report_char_count(count_letters(text))
+    print("----- End of Report -----")
 
 def get_book_text(book_path):
     with open(book_path) as bp:
@@ -27,7 +28,18 @@ def count_letters(text):
             count_chars[char] = 1 + count_chars.get(char, 0)
     return count_chars
 
+def sort_on(d):
+    return d["num"]
+
+def sorted_dictionary(dictionary):
+    sorted_arr = []
+    for ch in dictionary:
+        sorted_arr.append({"char": ch, "num": dictionary[ch]})
+    sorted_arr.sort(reverse=True, key=sort_on)
+    return sorted_arr
+
 def report_char_count(char_dictionary):
+    char_dict_sorted = sorted_dictionary(char_dictionary)
     for letter, count in char_dictionary.items():
         if letter.isalpha():
             print(f"The '{letter}' character was found {count} times")
